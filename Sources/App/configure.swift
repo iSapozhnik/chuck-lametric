e26b5bd5047c56496ddc16c67aa79c444d6d7d8c
Ivan Sapozhnik <sapozhnik.ivan@gmail.com>
2019-12-07 15:51:10 +0100

    Clean up

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -24,9 +24,4 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var databases = DatabasesConfig()
     databases.add(database: sqlite, as: .sqlite)
     services.register(databases)
-
-    // Configure migrations
-    var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
-    services.register(migrations)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,24 +1,5 @@
 import Vapor
 
-/*
- {
-     "frames": [
-         {
-             "icon":"i3219",
-             "text":"3"
-         }
-     ]
- }
- 
- {
- "icon_url" : "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
- "id" : "h1wmJ-T6SzmLyJjtJRV0cQ",
- "url" : ""
- "value" : "Chuck Norris recently guested on Hardcore Pawn, where he sold some pocket lint for 8.5 million dollars. Then he brutally roundhose kicked the owner for not actually having any porn."
- }
- 
- */
-
 struct Frame: Codable, Content {
     var frames: [Response]
 }
@@ -46,15 +27,4 @@ public func routes(_ router: Router) throws {
 
         return chuckFact
     }
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
